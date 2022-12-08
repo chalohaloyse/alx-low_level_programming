7ffff9f59b019066ba90e389bc4aa5d38b6b44fb
chalohaloyse <chalohaloyse@gmail.com>
2022-12-08 13:41:14 -0800

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,10 +10,9 @@
  */
 
 int main(void)
-{
 
-	puts("\"Programing is like building a multilingual puzzle");
-    	return (0);
+{
+	printf("\"with proper grammar, but the outcome is a piece of art");
+	return (0);
 
 }
-
