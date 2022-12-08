f3f3f5426e86576800cb7f2c081a22c7e89de07d
chalohaloyse <chalohaloyse@gmail.com>
2022-12-08 14:30:44 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,18 +2,13 @@
 
 
 /**
- *
- * main - Entry point
- *
+ * main - Main block
  * Return: Always 0 (Success)
- *
  */
 
 int main(void)
 {
-
 	puts("\"Programing is like building a multilingual puzzle");
     	return (0);
-
 }
 
