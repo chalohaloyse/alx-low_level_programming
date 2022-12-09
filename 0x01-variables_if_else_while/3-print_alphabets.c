0ddaa58097fd7c17343f518f5d70f3cee06d768f
chalohaloyse <chalohaloyse@gmail.com>
2022-12-09 12:48:41 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - retruns alphabets
+ * main - retrun alphabets both lower and upper cases
  *
- * Returns: Always 0 (Success)
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int ch = 'a';
+	int CH = 'A';
 
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+
 	putchar('\n');
 	return (0);
 }
