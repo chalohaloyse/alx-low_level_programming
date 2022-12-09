45dd85bfb96973c3679ffde6cabb31aeb2b8a59d
chalohaloyse <chalohaloyse@gmail.com>
2022-12-09 12:30:43 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - retruns alphabets
+ *
+ * Returns: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
