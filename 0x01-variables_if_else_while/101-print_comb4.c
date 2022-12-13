e49570b22ab2fd9a761edf6e1f995c30324a4187
chalohaloyse <chalohaloyse@gmail.com>
2022-12-13 09:50:05 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,7 +17,8 @@ int main(void)
 		for (tens = '0'; tens <= '9'; tens++)
 		{
 			for (ones = '0'; ones <= '9'; ones++)
-				if (!((ones == tens) || (tens == hundreds) || tens > ones) || (hundreds > tens))
+				if (!((ones == tens) || (tens == hundreds) ||
+							tens > ones) || (hundreds > tens))
 				{
 					putchar(hundreds);
 					putchar(tens);
