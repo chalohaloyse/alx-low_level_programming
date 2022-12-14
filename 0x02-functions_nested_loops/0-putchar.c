ab9f75a782597f575765977649553b546f0f7a13
chalohaloyse <chalohaloyse@gmail.com>
2022-12-13 19:11:33 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	-Putchar('_');
+	-Putchar('p');
+	-Putchar('u');
+	-Putchar('t');
+	-Putchar('c');
+	-Putchar('h');
+	-Putchar('a');
+	-Putchar('r');
+	-Putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H_INCLUDE
+#define HOLBERTON_H_INCLUDE
+
+int _putcher(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int m);
+void print_times_table(int m);
+void num(int r, int m);
+
+#endif
