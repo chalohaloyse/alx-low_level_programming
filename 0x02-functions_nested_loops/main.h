bec1d688a3eeb8fd6f80d6925ad996b4287c9933
chalohaloyse <chalohaloyse@gmail.com>
2022-12-13 19:24:43 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef HOLBERTON_H_INCLUDE
 #define HOLBERTON_H_INCLUDE
 
-int _putcher(char);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
