fe5c5aa179b285dd5b95c16095d57084d84665ec
chalohaloyse <chalohaloyse@gmail.com>
2022-12-14 10:00:21 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,22 @@
 #include "main.h"
+
+/**
+ * main - Prints -putchar followed by a new line
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	char text[8] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
