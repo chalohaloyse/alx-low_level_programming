a3159c806e03b0b65d7e117ee12a4751c66975b7
chalohaloyse <chalohaloyse@gmail.com>
2022-12-13 19:22:11 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,15 @@
 
 int main(void)
 {
-	_Putchar('_');
-	_Putchar('p');
-	_Putchar('u');
-	_Putchar('t');
-	_Putchar('c');
-	_Putchar('h');
-	_Putchar('a');
-	_Putchar('r');
-	_Putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
