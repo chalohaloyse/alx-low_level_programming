918b88e24f6aaa0d05ebd77e99b4d728e04b1375
chalohaloyse <chalohaloyse@gmail.com>
2022-12-14 11:53:11 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,10 @@ int print_last_digit(int n)
 {
 	int last;
 
-	last = 'n' % 10;
+	last = n % 10;
 	if (last < 0)
 	{
-		last = last + -1;
+		last *= -1;
 	}
 	_putchar(last + '0');
 	return (last);
