d87f755da2bca71c0754b67f0135e26db32a0100
chalohaloyse <chalohaloyse@gmail.com>
2022-12-20 10:16:16 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,19 +9,17 @@
 
 void puts_half(char *str)
 {
-	int len = 0;
+	int a, n, longi;
 
-	while (*str != '\0')
-	{
-		len++;
-		str++;
-	}
-	str -= (len / 2);
-	while (str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
+	longi = 0;
 
+	for (a = 0; str[a] != '\0'; a++)
+		longi++;
+	n = (longi / 2);
+	if ((longi % 2) == 1)
+		n = ((longi + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
 	_putchar('\n');
 }
