bab83d166975ade063a5168b95d28c186d2c8f43
chalohaloyse <chalohaloyse@gmail.com>
2022-12-20 08:41:33 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts2 - function should print only one character out of two
+ * @str: input
+ * Return: print
+ */
+
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2 == 0)
+		{
+			putchar(str[0]);
+		}
+	}
+	putchar('\n');
+}
