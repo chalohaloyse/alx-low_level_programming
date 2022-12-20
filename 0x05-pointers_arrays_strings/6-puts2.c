178ac32a9e373dda3c5199d48767b5caaccfbfcd
chalohaloyse <chalohaloyse@gmail.com>
2022-12-20 10:02:45 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,23 +8,15 @@
 
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+	int i = 0;
 
-	while (*y != '\0')
+	for (; str[i] != '\0'; i++)
 	{
-		y++;
-		longi++;
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
 	}
-	t = longi - 1;
-	for (o = 0; o <= t; o++)
-	{
-		if (o % 2 == 0)
-		{
-			_putchar(str[0]);
-		}
-	}
+
 	_putchar('\n');
 }
