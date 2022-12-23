5db441d477c93d363c1d3ab464946c4bad61e9c7
chalohaloyse <chalohaloyse@gmail.com>
2022-12-22 18:52:27 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,23 +9,30 @@
 
 char *cap_string(char *str)
 {
-	int index = 0;
+	int i = 0;
 
-	while (str[index])
+	while (str[1])
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-		str[index - 1] == '\t' || str[index - 1] == '\n' ||
-		str[index - 1] == ',' || str[index - 1] == '.' ||
-		str[index - 1] == '!' || str[index - 1] == '?' ||
-		str[index - 1] == " " || str[index - 1] == '(' ||
-		str[index - 1] == ')' || str[index - 1] == '{' ||
-		str[index - 1] == '}' || index == 0)
-			str[index] -= 32;
-
-		index++;
+		while (!(str[1] >= 'a' && str[1] <= 'z'))
+			i++;
+		if (str[i - 1] == ' ' ||
+			str[i - 1] == '\t' ||
+			str[i - 1] == '\n' ||
+			str[i - 1] == ',' ||
+			str[i - 1] == ';' ||
+			str[i - 1] == '.' ||
+			str[i - 1] == '!' ||
+			str[i - 1] == '?' ||
+			str[i - 1] == '"' ||
+			str[i - 1] == '(' ||
+			str[i - 1] == ')' ||
+			str[i - 1] == '{' ||
+			str[i - 1] == '}' ||
+			i == 0)
+				str[i] -= 32;
+		i++;
 	}
+
 	return (str);
 }
+
