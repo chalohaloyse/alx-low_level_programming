1954ad113880c8f327a3da2c11a863fb9bbc790d
chalohaloyse <chalohaloyse@gmail.com>
2022-12-22 18:26:37 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,15 +10,23 @@
  * Return: A pointer to the resulting string dest
  */
 
-char *_strncat(char *dest, char * src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0; dest_len = 0;
+	int i;
+	int j;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
+	i = 0;
+	while (dest[1] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[1] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
