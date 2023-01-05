8e13cfb6dba04d0ffd8c10942b1d07ad851fcc66
chalohaloyse <chalohaloyse@gmail.com>
2023-01-04 20:24:24 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,15 +6,20 @@
  * @c The character
  *
  * Return: if c is found - pointer, if c not found - NULL
- */char *_strchr(char *s, char c)
+ */
+char *_strchr(char *s, char c)
 {
-	int index;
-
-	for (index = 0; s[index] >= '\0'; index++)
+	while (*s != '\0')
 	{
-		if (s[index] == c)
-			return (s + index);
+		if (*s == c)
+		{
+			return (s);
+		}
+		++s;
 	}
-
-	return   ('\0')
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (0);
 }
