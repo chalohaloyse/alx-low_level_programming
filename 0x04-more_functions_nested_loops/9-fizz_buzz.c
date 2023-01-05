e79258747ab3f5f8876254d2bee7ed4a79b606c7
chalohaloyse <chalohaloyse@gmail.com>
2023-01-04 18:38:24 -0800

    0-memset.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints Buzz each numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
+	{
+		if ((n %3 == 0) && n % 5 == 0))
+		{
+			prinf
---
+++ README.md
@@ -0,0 +1,1 @@
+My Repository on C Languange
