4195fb9fc75572ea7f4130b4e440a1a380ccc197
chalohaloyse <chalohaloyse@gmail.com>
2023-01-04 19:01:28 -0800

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destinantion
+ * @src: source
+ * @n: max bytes to use
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; n > 0; i--)
+	{
+		dest[i] = scr[i]
+	}
+
+	return (dest);
+}
