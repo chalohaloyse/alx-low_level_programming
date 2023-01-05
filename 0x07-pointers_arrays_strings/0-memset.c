c1de38823188417c66b05ad6618197bd4938990c
chalohaloyse <chalohaloyse@gmail.com>
2023-01-04 19:47:19 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,16 +3,15 @@
 /**
  * _memset - Fills the first n bytes of memory area
  * pointed to by @s with constant byte @c
- * @s: A pointer to the meomory area to be filled
- * @c: Character to fill the memory
+ * @s: Source string
+ * @b: Character to fill the memory
  * @n: The number of bytes to be filled
  *
  * Description_memset: over there
  *
- * Return: Pointer tofilled memory area @s
+ * Return:New string
  */
-
-void *_memset(void *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
