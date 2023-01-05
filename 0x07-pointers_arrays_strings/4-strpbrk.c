302f3e05094c2c70d6b9b35c158bc7ff3d753ffe
chalohaloyse <chalohaloyse@gmail.com>
2023-01-04 20:55:36 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,7 +13,7 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; *(s + i); i++)
 	{
-		for  (j = 0; *(accept + j) j++)
+		for  (j = 0; *(accept + j); j++)
 		{
 		if (*(s + i) == *(accept + j))
 		{
