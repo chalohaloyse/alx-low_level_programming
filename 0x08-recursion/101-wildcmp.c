09a2fcdbd728b2501672f14c1c6214c58a720e78
chalohaloyse <chalohaloyse@gmail.com>
2023-01-06 13:17:24 -0800

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,27 @@
+/**
+ * wildcmp - compare strings
+ * @s1: pointer one
+ * @s2: pointer two
+ * Return: 0
+ */
+int wildcmp(char *s1, *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 = '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
