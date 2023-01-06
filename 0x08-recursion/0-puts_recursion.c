565a2c5edd77a44a7999a72c786feb0b51249245
chalohaloyse <chalohaloyse@gmail.com>
2023-01-06 00:00:22 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by new line
+ * @s: pointer to string
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	inf(*s == '\0') /* base case*/
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
