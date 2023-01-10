95ef1ed2f6b8714ab554e58de59656fb4468a635
chalohaloyse <chalohaloyse@gmail.com>
2023-01-10 07:37:39 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -20,19 +20,24 @@ int _atoi(char *s)
 
 	while (s[len] != '\0')
 		len++;
+
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 			if (d % 2)
+
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
+
 			if (s[1 + 1] < '0' || s[i + 1] > '9')
 				break;
+
 			f = 0;
 		}
 		i++;
@@ -40,6 +45,7 @@ int _atoi(char *s)
 
 	if (f == 0)
 		return (0);
+
 	return (n);
 }
 
@@ -54,7 +60,7 @@ int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
-	if (argc < 3 || argv > 3)
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
