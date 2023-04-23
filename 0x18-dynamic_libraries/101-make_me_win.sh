ad1a25c9455aaf25c51f036bf2cc40994568905c
chalohaloyse <chalohaloyse@gmail.com>
2023-04-23 05:09:08 -0700

    update

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -O /tmp/libinjection.so https://github.com/chalohaloyse/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libinjection.so
+export LD_PRELOAD=/tmp/libinjection.so
